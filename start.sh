4ef53f27503d73335bd605f82fd98d0a8ef72edf
Huanqd <609139528@qq.com>
2018-10-17 16:24:05 +0800

    部署脚本

---
+++ start.sh
@@ -0,0 +1,1 @@
+nohup java -Dspring.profiles.active=test -jar target/service-wx.jar >/dev/null 2>&1 &
---
+++ vlog.sh
@@ -0,0 +1,3 @@
+echo -------------------------------------------------------分界线:$(date +%Y-%m-%d%t%T)
+
+tailf /data/logs/service-wx/$(date +%Y-%m-%d).log
